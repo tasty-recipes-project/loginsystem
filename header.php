ba4f658d64e63a98b13c6919c195254e6912e877
Florian Rabe <rabe.florian85@googlemail.com>
2020-11-29 11:26:15 +0100

    Upload des Login Systems Version1

---
+++ header.php
@@ -0,0 +1,42 @@
+<!Doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>tastyrecipes</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+        <div class="row logo">
+        <div class="col-lg-2">
+            <img src="images/logo_tastyrecipes.jpg" height="100px" width="100px">
+        </div>
+        <div class="col-lg-5">
+        <header>
+            <nav>
+                <ul class="menu">
+                    <li>Startseite</li>
+                    <li>Rezepte</li>
+                    <li>Wissenwertes</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>    
+        </header>
+        </div>
+        <div class="col-lg-5">
+            <form action="test.php" method="post" class="login_area">
+                <input type="text" name="username" placeholder="Name/Emailadresse">
+                <input type="password" name="passwort" placeholder="Passwort">
+                <p>Noch kein Konto? Dann aber fix Registrieren</p>
+            </form>
+        </div>
+        </div>
+        <div class="row">
+                <div class="col-lg-12 header_image">
+                    <img src="images/header_kochbar1600x300.jpeg">
+                </div>
+        </div>
+
+
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require('header.php')
+?>
+
+<main class="main_content">
+    <h3>TestWebseite</h3>
+</main>
+
+<?php
+    require('footer.php')
+?>
